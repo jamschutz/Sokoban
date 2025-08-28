3d7dacb6783019d5a025586ee1ed87ed62664f59
jamschutz <52084893+jamschutz@users.noreply.github.com>
2025-08-28 15:04:56 -0400

    basic movement

--- Assets/Scripts/GridController.cs
+++ Assets/Scripts/GridController.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[RequireComponent(typeof(Tilemap))]
-[RequireComponent(typeof(TilemapCollider2D))]
 [RequireComponent(typeof(Grid))]
 public class GridController : MonoBehaviour
 {
-    private Tilemap tilemap;
-    private Grid grid;
+    // singleton instance
+    public static GridController instance;
+
+    private Grid _grid;
 
 
     private void Start()
     {
-        tilemap = GetComponent<Tilemap>();
-        grid = GetComponent<Grid>();
+        // instance already exists -- can only be one singleton!
+        if (instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        GridController.instance = this;
+        _grid = GetComponent<Grid>();
     }
 
 
     public Vector3 GridToWorldPos(int x, int y)
     {
-        return grid.CellToWorld(new Vector3Int(x, y, 0));
+        return _grid.CellToWorld(new Vector3Int(x, y, 0));
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private int _x;
+    private int _y;
+
+    private void Start()
+    {
+        _x = 0;
+        _y = 0;
+        Move(0, 0);
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(0, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(0, -1);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(-1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(1, 0);
+        }
+
+    }
+
+
+    private void Move(int x, int y)
+    {
+        _x += x;
+        _y += y;
+        transform.position = GridController.instance.GridToWorldPos(_x, _y);
+    }
+}
